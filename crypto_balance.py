2f2355189cbefde7fec77e6ecd08cbe6e52fa27d
Andrew Pierce <andrewpierce@Andrews-iMac.fios-router.home>
2017-12-17 11:11:23 -0500

    Adding DENT

--- crypto_balance.py
+++ crypto_balance.py
@@ -28,18 +28,24 @@ class CryptoBalance(Resource):
 		xrp_price = r.json()
 		xrp_price = float(xrp_price[0]['price_usd'])
 
+		r = requests.get('https://api.coinmarketcap.com/v1/ticker/dent/')
+		dent_price = r.json()
+		dent_price = float(dent_price[0]['price_usd'])		
+
 		btc_amt = float(request.args.get('btc'))
 		xrp_amt = float(request.args.get('xrp'))
 		ltc_amt = float(request.args.get('ltc'))
 		eth_amt = float(request.args.get('eth'))
+		dent_amt = float(request.args.get('dent'))
 
 		btc_usd = btc_price*btc_amt
 		ltc_usd = ltc_price*ltc_amt
 		eth_usd = eth_price*eth_amt
 		xrp_usd = xrp_price*xrp_amt
-		total_usd = btc_usd + ltc_usd + xrp_usd + eth_usd
+		dent_usd = dent_price*dent_amt
+		total_usd = btc_usd + ltc_usd + xrp_usd + eth_usd + dent_usd
 		profit = total_usd - invested
-		return {'invested_now':invested, 'total':total_usd, 'profit': profit, 'btc_price':btc_price, 'ltc_price':ltc_price, 'xrp_price':xrp_price, 'eth_price':eth_price}
+		return {'invested_now':invested, 'total':total_usd, 'profit': profit, 'btc_price':btc_price, 'ltc_price':ltc_price, 'xrp_price':xrp_price, 'eth_price':eth_price, 'dent_price': dent_price}
 
 
 
