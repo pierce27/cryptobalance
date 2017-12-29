51c1b2515596c595f44079d21ab6432c2a9913a7
Andrew Pierce <andrewpierce@Andrews-iMac.fios-router.home>
2017-12-29 13:41:20 -0500

    Rewrite in Node JS

--- cryptkeep.js
+++ cryptkeep.js
@@ -1,118 +1,74 @@
 var https = require('https'),
 sanitizeHtml = require('sanitize-html')
 var urls = ['https://api.coinmarketcap.com/v1/ticker/bitcoin/', 'https://api.coinmarketcap.com/v1/ticker/litecoin/', 'https://api.coinmarketcap.com/v1/ticker/ethereum/', 'https://api.coinmarketcap.com/v1/ticker/ripple/']
+var url = 'https://bittrex.com/api/v1.1/public/getmarketsummaries'
 var responses = [];
 var completed_requests = 0;
 
 exports.info = function(request, response){
-	console.log(request.body)
-	var btc_rate;
-	var ltc_rate;
-	var eth_rate;
-	var xrp_rate;
+	var userData = request.body
 
-	// for (i in urls) {
-	//     https.get(urls[i], function(res) {
-	//         responses.push(res);
-	//         completed_requests++;
-	//         if (completed_requests == urls.length) {
-	//             // All download done, process responses array
-	//             console.log(responses);
-	//             for (i in responses){
-	//             	var data = JSON.parse(responses[i].body)
-	//             	console.log(data)
-	//             }
-	//         }
-	//     });
-	// }	
-
-
-	urls.forEach(function(url) {
-	  var responses = [];
-	  https.get(url, function(res) {
+	https.get(url, function(res){
+		// console.log(res)
+	    var body = '';
 	    res.on('data', function(chunk){
-	      responses.push(chunk);
+	        body += chunk;
 	    });
-
 	    res.on('end', function(){
-	      if (completed_requests++ == urls.length - 1) {
-	        // All downloads are completed
-	        // console.log(responses)
-	        console.log('body:', responses.join());
-	      }      
+	        var markets = JSON.parse(body);
+	        var rates = getRates(markets.result)
+	        var investmentInfo = calcInvestment(rates, userData)
+	        console.log(investmentInfo)
+
+	        // console.log(markets.result[0])
+	        response.send(investmentInfo)
+
 	    });
-	  });
-	})
+	}).on('error', function(e){
+	      console.log("Got an error: ", e);
+	});
 
+}
 
+var getRates = function(marketData){
+	var rates = {}
 
-	// https.get(btc_url, function(res){
-	// 	// console.log(res)
-	//     var body = '';
-	//     res.on('data', function(chunk){
-	//         body += chunk;
-	//     });
-	//     res.on('end', function(){
-	//         var bitcoin = JSON.parse(body);
-	//         btc_rate = bitcoin[0].price_usd
-	//         // console.log("Got a response: ", bitcoin[0].price_usd);
-	//         // response.send(body)
-	//     });
-	// }).on('error', function(e){
-	//       console.log("Got an error: ", e);
-	// });
+	for (var i = marketData.length - 1; i >= 0; i--) {
+		var marketName = marketData[i].MarketName 
+		var currency = marketName.replace(/-.*/g, '')
+		var crypto = marketName.replace(/.*-/g, '')
+		if (currency == 'USDT') {
+			crypto = crypto.toLowerCase()
+			rates[crypto] = marketData[i].Ask
+		}
+	
+	}
+	return rates
+}
 
-	// // GET LTC Rate
-	// https.get(ltc_url, function(res){
-	// 	// console.log(res)
-	//     var body = '';
-	//     res.on('data', function(chunk){
-	//         body += chunk;
-	//     });
-	//     res.on('end', function(){
-	//         var litecoin = JSON.parse(body);
-	//         ltc_rate = litecoin[0].price_usd
-	//         // console.log("Got a response: ", litecoin[0].price_usd);
-	//         // response.send(body)
-	//     });
-	// }).on('error', function(e){
-	//       console.log("Got an error: ", e);
-	// });
+var calcInvestment = function(rates, userData){
+	var investmentInfo = {'details':{}, 'total':'', 'profit':'', 'invested_now':''}
+	var value;
+	var price; 
+	var amount;
+	investmentInfo.total = 0;
+	for(var key in userData){
+		if(key == 'invested'){
+			investmentInfo.invested_now = parseInt(userData[key])
+		} else {
+			console.log(key)
+			amount = userData[key];
+			// console.log(rates[key])
+			price = rates[key];
+			// console.log(price)
+			value = amount*price;
+			investmentInfo.total += value;
 
-	// // Get ETH rate
-	// https.get(eth_url, function(res){
-	// 	// console.log(res)
-	//     var body = '';
-	//     res.on('data', function(chunk){
-	//         body += chunk;
-	//     });
-	//     res.on('end', function(){
-	//         var ethereum = JSON.parse(body);
-	//         eth_rate = ethereum[0].price_usd
-	//         // console.log("Got a response: ", ethereum[0].price_usd);
-	//         // response.send(body)
-	//     });
-	// }).on('error', function(e){
-	//       console.log("Got an error: ", e);
-	// });		
+			investmentInfo.details[key] = {'price': price, 'amount':amount, 'value':value}			
+		}
 
-	// 	// Get ETH rate
-	// https.get(xrp_url, function(res){
-	// 	// console.log(res)
-	//     var body = '';
-	//     res.on('data', function(chunk){
-	//         body += chunk;
-	//     });
-	//     res.on('end', function(){
-	//         var ripple = JSON.parse(body);
-	//         xrp_rate = ripple[0].price_usd
-	//         console.log("Got a response: ", xrp_rate);
-	//         // response.send(body)
-	//     });
-	// }).on('error', function(e){
-	//       console.log("Got an error: ", e);
-	// });	
-
-	// console.log(xrp_rate)
-
+	}
+	investmentInfo.profit = investmentInfo.total - investmentInfo.invested_now
+	console.log(investmentInfo)
+	return(investmentInfo)
 }
--- resources/controllers.js
+++ resources/controllers.js
@@ -1,38 +1,19 @@
-var cryptKeeper = angular.module('cryptKeeper', ['ngSanitize']);
+var cryptKeeper = angular.module('cryptKeeper', ['ngCookies']);
 
+cryptKeeper.controller('cryptCtrl', function CryptCtrl($scope, $http, $cookies) {	
 
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip()
-})
-
-cryptKeeper.controller('cryptCtrl', function CryptCtrl($scope, $http) {	
-
-	// GET DATA FOR MTA SERVICE STATUS
-	$http({method: 'GET', url: '/cryptkeep/all'}).
-	success(function(data, status) {
-		$scope.litecoin = data[0]
-	  console.log(data);	  
-	}).
-	error(function(data, status) {
-		// TODO Alert if error
-	  
-	});	
-
-		// SIGN IN 
-	$scope.getBalance = function(){
-		// Create user object to send to server
-		var cryptInfo = {
-			'invested':this.invested,
-			'btc': this.btc,
-			'ltc': this.ltc,
-			'eth': this.eth,
-			'xrp': this.xrp
-		}
-	    console.log(cryptInfo)
+	$scope.hideTable = true;
+	
+	var getBalance = function(postData)	{
 
 	    // Attempt to login to server
-		$http.post('/cryptkeep', cryptInfo).
+		$http.post('/cryptkeep', postData).
 		success(function(data, status) {
+			console.log(data)
+			$scope.cryptData = data
+			// $scope.$apply()
+			// $cookies.put('test', 'thisIsATest')
+			$scope.hideTable = false;
 			console.log(data)
 
 		}).
@@ -43,10 +24,49 @@ cryptKeeper.controller('cryptCtrl', function CryptCtrl($scope, $http) {
 			
 		  
 		});			
-	}	
 
+	}
+	
 
+	if(!$cookies.cryptkeep){
+		console.log('no cookie')
+	} else {
+		$scope.hideForm = true;
+		var data = $cookies.cryptkeep;
+		getBalance(data);
+	}
 
+	$scope.getBalance = function(){
+		
+		// Create user object to send to server
+		var cryptInfo = {
+			'invested':this.invested,
+			'btc': this.btc,
+			'ltc': this.ltc,
+			'eth': this.eth,
+			'xrp': this.xrp,
+			'bcc': this.bcc
+		};
+	    
+	    $cookies.cryptkeep = JSON.stringify(cryptInfo);
+	    $scope.hideForm = true;
+	    getBalance(cryptInfo);
+
+		
+	}
+
+	$scope.newInfo = function(){
+		$scope.invested = $scope.cryptData.invested_now
+		$scope.btc = $scope.cryptData.btc[0]
+		$scope.ltc = $scope.cryptData.ltc[0]
+		$scope.eth = $scope.cryptData.eth[0]
+		$scope.xrp = $scope.cryptData.xrp[0]
+		$scope.bcc = $scope.cryptData.bcc[0]
+		console.log('clicked')
+		$cookies.cryptkeep = false;
+		$scope.hideForm = false;
+		$scope.cryptData = {}
+	}
 
 
 
